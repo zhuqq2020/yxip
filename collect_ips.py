73ba37c3ead2875e72143f1ba0d966679f7291c8
zhuqq2020 <72717269+zhuqq2020@users.noreply.github.com>
2025-06-29 21:39:21 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -6,7 +6,9 @@ import os
 # 目标URL列表
 urls = ['https://api.uouin.com/cloudflare.html', 
         'https://ip.164746.xyz',
-        'https://api.uouin.com/cloudflare.html'
+        'https://vps789.com/cfip',
+        'https://stock.hostmonit.com/CloudFlareYes',
+        'https://cf.090227.xyz'
         ]
 
 # 正则表达式用于匹配IP地址
